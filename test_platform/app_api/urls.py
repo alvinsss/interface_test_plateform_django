e7ae8d979c3c3d7880d126d821e7e09c378749f0
alvin <6449694@qq.com>
2021-08-23 17:43:20 +0800

    步骤8 app_api下urls.py创建项目接口及views下rest_framework格式定义

--- test_platform/app_api/urls.py
+++ test_platform/app_api/urls.py
@@ -5,10 +5,12 @@
 # @Software: PyCharm
 from django.conf.urls import url
 from django.urls import path, re_path
-from . import views
+from  app_api.views.project_view import index
+from  app_api.views.project_view import ProjectView
 #真正的Django项目中，可能有多个应用程序，Django如何区分它们之间的URL名称,向您
 # 的URLconf添加名称空间，app_name要设置应用程序命名空间，模板文件需要同步加上名称空间
 app_name = 'app_api'
 urlpatterns = [
-    path('', views.index ),
+    path('',index ),#http://127.0.0.1:8000/api/
+    path('v1/project/',ProjectView.as_view()),
 ]
---
+++ test_platform/app_api/views/project_view.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+# @Time    : 2021/8/23 17:25
+# @Author  : alvin
+# @File    : project_view.py
+# @Software: PyCharm
+
+from django.shortcuts import render
+from rest_framework.views import  APIView
+from django.http import JsonResponse
+
+def index(request):
+    print("rest_app index!")
+    return JsonResponse( {"msg":"it works!"})
+
+class ProjectView(APIView):
+
+    def get(self, request, *args, **kwargs):
+        """
+        查询 uid有值查单条 没值查询全部数据
+        """
+        return JsonResponse( {"msg":"pro test get"})
+    def post(self, request, *args, **kwargs):
+        """
+        添加
+        """
+        return JsonResponse( {"msg":"it works!"})
+    def put(self, request, *args, **kwargs):
+        """
+        更新
+        """
+        return JsonResponse( {"msg":"it works!"})
+
+    def delete(self, request, *args, **kwargs):
+        """
+        删除
+        """
+        return JsonResponse( {"msg":"it works!"})
+
