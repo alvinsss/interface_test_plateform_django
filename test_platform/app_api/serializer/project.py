9644c817773e4ca95737c05be57a5c403f056c08
alvin <6449694@qq.com>
2021-08-23 18:33:01 +0800

    步骤10 serializer使用父类create  进行表数据写入

--- test_platform/app_api/serializer/project.py
+++ test_platform/app_api/serializer/project.py
@@ -4,7 +4,18 @@
 # @File    : project.py
 # @Software: PyCharm
 from rest_framework import serializers
+from app_api.models.project_model import  Project
 
+aatype=[1,2,3]
 class ProjectValidator(serializers.Serializer):
-    name = serializers.CharField(required=True,error_messages={"required":"name不能为空"})
-
+    name = serializers.CharField(required=True,
+                                 error_messages={"required":"name不能为空",
+                                 "invalid":"类型不对",
+                                 "max_length":"长度不能大于50"})
+    describe = serializers.CharField(required=True,error_messages={"required":"describe不能为空"})
+    status = serializers.BooleanField(required=True,error_messages={"required":"status不能为空"})
+    # aatype = serializers.ChoiceField(choices=aatype)#枚举
+    def create(self, validated_data):
+        '''创建'''
+        project = Project.objects.create(**validated_data)
+        return project
--- test_platform/app_api/views/project_view.py
+++ test_platform/app_api/views/project_view.py
@@ -25,11 +25,13 @@ class ProjectView(APIView):
         添加
         """
         name = request.data.get("name","")
+        status = request.data.get("status","")
+        describe = request.data.get("describe","")
         print(request.data)
         val = ProjectValidator(data=request.data)
         print(val)
-        if val.is_valid():
-            val.save()
+        if val.is_valid():#判断验证的字段是否都对，参考serizlizer.project定义
+            val.save() #保存数据
         else:
             print(val.errors)
             return JsonResponse({"msg":"添加","error":val.errors})
